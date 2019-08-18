f721d43843df4765919fe7f59a4a5b4f6c10594a
Marc Vilella <mvilella@obsa.ad>
2019-08-18 16:43:09 +0200

    Authenticate users in database. Add /users/me endpoint to retrieve authenticated user information

--- ns/auth.py
+++ ns/auth.py
@@ -1,6 +1,9 @@
 from flask_restplus import Namespace, Resource, reqparse
 import flask_jwt_extended as jwt
 
+from models.users import User
+
+
 ns = Namespace('auth', description='Authorization')
 
 @ns.route('')
@@ -13,12 +16,15 @@ class Authentication(Resource):
 	# Validate user credentials and obtain access + refresh tokens
 	def post(self):
 		args = self.parser.parse_args()
-		if args['username'] == 'test' and args['password'] == 'test':
-			return {
-				'access_token': jwt.create_access_token(identity=args['username']),
-				'refresh_token': jwt.create_refresh_token(args['username'])
-			}
-		return { 'message': 'Invalid credentials' }, 401
+		user = User.query.filter_by(username=args['username']).first()
+		if user is not None:
+			if user.check_password(args['password']):
+				return {
+				 	'access_token': jwt.create_access_token(identity=args['username']),
+					'refresh_token': jwt.create_refresh_token(args['username'])
+				}
+			return { 'message': 'Invalid password' }, 401
+		return { 'message': 'Invalid username' }, 401
 
 	# Renew access token from refresh token
 	@jwt.jwt_refresh_token_required
--- ns/users.py
+++ ns/users.py
@@ -1,4 +1,5 @@
 from flask_restplus import Namespace, Resource
+import flask_jwt_extended as jwt
 
 from models.users import User
 import schemas.users as schemas
@@ -11,6 +12,18 @@ class ListUsers(Resource):
 	@ns.marshal_with(schemas.list_users)
 	def get(self):
 		return User.query.all()
+
+
+@ns.route('/me')
+class SelfUser(Resource):
+	@jwt.jwt_required
+	@ns.marshal_with(schemas.self_user)
+	def get(self):
+		username = jwt.get_jwt_identity()
+		user = User.query.filter_by(username=username).first()
+		if user is None:
+			ns.abort(404, 'User ' + username + ' does not exist')
+		return user
 
 
 @ns.route('/<string:username>')
